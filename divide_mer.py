783466b1ef5c7a852e1fb1a095b05d72d15cdce6
Ozge Yoluk <ozgeyoluk86@gmail.com>
2016-11-11 15:40:01 +0100

    Returns the CA of the first assembly, things left pdbwriting

--- divide_mer.py
+++ divide_mer.py
@@ -1,14 +1,23 @@
 #We need to seperate the biological assemblies
 #
 import numpy as np
+import logging
 
 def divide_mer(ca,compnd,mer,missinginfo):
     slice=len(compnd) / mer
-    divide_mer=np.array_split(ca,slice)
-    if all(missinginfo.values()==1):
-        divide_mer=np.array_split(ca,slice)[0]
-        logging.info('I will continue with the first biological assembly')
-    else:
-        
-
-    return divide_mer[0]
+    nrba={}
+    for i in range(slice):
+        nrba[i]=compnd[i*mer:i*mer+mer]
+    batouse=[]
+    for ba in nrba:
+        bainfo=0
+        for ch in nrba[ba]:
+          bainfo=bainfo+missinginfo[ch]
+        if bainfo==mer:
+            batouse.append(ba)
+            continue
+        else:
+            logging.info('All the assemblies are broken')
+    divide_mer=np.array_split(ca,slice)[batouse[0]]
+    logging.info('I will continue with the %i biological assembly' %(batouse[0]+1))
+    return divide_mer
