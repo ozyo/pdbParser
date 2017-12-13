801b98416e4cbea1cbaddbffa4f15328004ecd5d
ozyo <ozgeyoluk86@gmail.com>
2017-12-13 16:42:56 -0500

    Important fix to segment class for charmm segments

--- pdbparser/sep_seg.py
+++ pdbparser/sep_seg.py
@@ -19,18 +19,24 @@ class Segsep(object):
         self.hoh=coord[(coord['resname'] == 'HOH') | (coord['resname'] == 'TIP3') | (coord['resname'] == 'SOL')]
         self.rest=coord[(coord['resname'] != 'HOH') & (coord['resname'] != 'TIP3') | (coord['resname'] != 'SOL') ]
         self.chains=set(self.rest['ch'].tolist())
-        self.segids=set(self.rest['segid'].tolist())
+        try:
+            len(self.rest['segid'])
+            self.segids=set(self.rest['segid'].tolist())
+        except ValueError:
+            'No segments'
         self.coorsegs=np.empty_like(coord)
         remove=['element','charge']
         for name in remove:
             self.coorsegs=remove_field_name(self.coorsegs,name)
-        #self.coorsegs=numpy.lib.recfunctions.append_fields(self.coorsegs, 'segid', dtypes='S5', usemask=False, asrecarray=True)
+        self.coorsegs=numpy.lib.recfunctions.append_fields(self.coorsegs, 'segid',[]*len(self.coorsegs), dtypes='S5', usemask=False, asrecarray=True)
         if len(self.chains) == 0:
             self.chains=['A']
             print 'Hey no chain found, I set everything to A'
-
+        
     def sep_segs(self,coord):
-        if len(self.segids) == 0:
+        try:
+            self.segids
+        except AttributeError:
             remove=['element','charge']
             for chain in self.chains:
                 seg_ch=self.rest[self.rest['ch']==chain]
@@ -38,13 +44,14 @@ class Segsep(object):
                 for name in remove:
                     seg_less=remove_field_name(seg_less,name)
                 seg_id=numpy.lib.recfunctions.append_fields(seg_less, 'segid', ['SEG'+chain]*len(seg_less), dtypes='S5', usemask=False, asrecarray=True)
-                self.coorsegs=np.concatenate(self.coorsegs,seg_id)
+                self.coorsegs=np.concatenate((self.coorsegs,seg_id))
             seg_less_wat=self.hoh
             if len(seg_less_wat) > 0:
                 for name in remove:
                     seg_less_wat=remove_field_name(seg_less_wat,name)
                 seg_id_wat=numpy.lib.recfunctions.append_fields(seg_less_wat, 'segid', ['SEGW']*len(seg_less_wat), dtypes='S5', usemask=False, asrecarray=True)
-                self.coorsegs=np.concontenate(self.coorsegs,seg_id_wat)
+                self.coorsegs=np.concatenate((self.coorsegs,seg_id_wat))
+            self.coorsegs=self.coorsegs[self.coorsegs['segid']!='N/A']
             return self.coorsegs
         else:
             return coord
