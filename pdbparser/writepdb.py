a3c987c50a674815cf1707c492de6ba6c0977c29
ozyo <ozgeyoluk86@gmail.com>
2018-01-18 14:58:38 -0500

    Writes out END statement

--- pdbparser/writepdb.py
+++ pdbparser/writepdb.py
@@ -15,14 +15,16 @@ def crysol_sanity_check(coord):
             
 def write(coord,file,coortype,bysegid):
     if bysegid is False:
-        if coortype == "charmm":
-            np.savetxt(file,coord,fmt='ATOM  %5s %-4s%1s%-4s%1s%5s   %8s%8s%8s%6s%6s      %-5s',delimiter='')
-        elif coortype == "crysol":
-            #This is needed to keep the atom name the way crysol likes. '  C ' or '  N1' not 'N1  ' or '   C'
-            coord=crysol_sanity_check(coord)
-            np.savetxt(file,coord,fmt='ATOM  %5s %4s%1s%-4s%1s%5s   %8s%8s%8s%6s%6s     %-5s',delimiter='')
-        else:
-            np.savetxt(file,coord,fmt='ATOM  %5s %-4s%1s%-4s%1s%5s   %8s%8s%8s%6s%6s          %-2s%2s',delimiter='')
+        with open(file,'w') as output:
+            if coortype == "charmm":
+                np.savetxt(output,coord,fmt='ATOM  %5s %-4s%1s%-4s%1s%5s   %8s%8s%8s%6s%6s      %-5s',delimiter='')
+            elif coortype == "crysol":
+                #This is needed to keep the atom name the way crysol likes. '  C ' or '  N1' not 'N1  ' or '   C'
+                coord=crysol_sanity_check(coord)
+                np.savetxt(output,coord,fmt='ATOM  %5s %4s%1s%-4s%1s%5s   %8s%8s%8s%6s%6s     %-5s',delimiter='')
+            else:
+                np.savetxt(file,coord,fmt='ATOM  %5s %-4s%1s%-4s%1s%5s   %8s%8s%8s%6s%6s          %-2s%2s',delimiter='')
+            np.savetxt(output,np.array('END').reshape(1,),fmt='%3s',delimiter='s')
     else:
         # Add the END stament to the end of the file
         segids=np.unique(coord['segid'])
