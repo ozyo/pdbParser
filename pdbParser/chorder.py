e6beb3c0b4a4b21e29fc4ef9239cafcb02a2cd1c
Ozge Yoluk <ozgeyoluk@proteinart.net>
2019-09-28 17:37:42 -0400

    Chain ordering, attempt to fix

--- pdbParser/chorder.py
+++ pdbParser/chorder.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from copy import deepcopy
 from pdbParser import parser as pP
@@ -11,11 +12,37 @@ def com(ca,chain=None):
     return(com)
 
 def direction(ca,chains):
+    chains=list(sorted(chains))
+    com_all=np.array(com(ca))
+    xyz=np.array(ca[['x','y','z']].tolist())
+    xyz=xyz-com_all
+    # For ordering https://github.com/pierrepo/principal_axes/blob/master/principal_axes.py
+    # and mdanalysis routines merged
+    inertia = np.dot(xyz.transpose(), xyz)
+    e_values, e_vectors = np.linalg.eig(inertia)
+    order = np.argsort(e_values)
+    eval3, eval2, eval1 = e_values[order]
+    axis3, axis2, axis1 = e_vectors[:, order].transpose()
+    rotxyz=np.array([axis1,axis2,axis3])
+    for i in range(xyz.shape[0]):
+        t=np.dot(rotxyz,xyz[i])
+        ca['x'][i]=round(t[0],3)
+        ca['y'][i]=round(t[1],3)
+        ca['z'][i]=round(t[2],3)
+    print("XYZ axis limits:\n   " , xyz[:,0].min(), "to", xyz[:,0].max() )
+    print("   " , xyz[:,1].min(), "to", xyz[:,1].max() )
+    print("   " , xyz[:,2].min(), "to", xyz[:,2].max())
+    #ca['x'][np.where(ca['x'])]=xyz[:,0]
+    #ca['y'][np.where(ca['y'])]=xyz[:,1]
+    #ca['z'][np.where(ca['z'])]=xyz[:,2]
+
     coms=[]
-    for ch in range(len(chains)):
-        if ch < 2:
-            coms.append(com(ca,chain=chains[ch]))
+    #for ch in range(len(chains)):
+    #    if ch < 2:
+    #        coms.append(com(ca,chain=chains[ch]))
     coms.append(com(ca))
+    coms.append(com(ca,chain=chains[0]))
+    coms.append(com(ca,chain=chains[1]))
     coms=np.array(coms)
     if np.linalg.det(coms) > 0:
         #If matrix determinant is positive, anticlockwise. Else clockwise
@@ -31,7 +58,9 @@ def change_direction(chlist):
 def direction_check(uni_list,chorderlist):
     directions={}
     for ids,uni in uni_list.items():
+        print(ids)
         directions[ids]=direction(uni,chorderlist[ids])
+    print(directions)
     acs=list(directions.values()).count('ac')
     cs=list(directions.values()).count('c')
     reorder=[]
@@ -64,6 +93,7 @@ def reorder(id_ch_dict,mer,cwd,altloc='A',tag=None):
         uni_list[ids]=pP.parse_ca(open(cwd+'/'+name,'r'),'NA',mer,altloc,list(sorted(id_ch_dict[ids])))
     neworders=direction_check(uni_list,id_ch_dict)
     for ids in uni_list:
+        #We could simply loop over neworders improve this part.
         if ids in neworders.keys():
             newchains=[]
             sep_chains=sepchains(uni_list[ids],id_ch_dict[ids])
@@ -76,7 +106,7 @@ def reorder(id_ch_dict,mer,cwd,altloc='A',tag=None):
                 newchains.append(tmp)
             merged=np.concatenate(newchains)
             wp.writeca(merged,cwd+'/'+'reordered_'+ids)
-
+    return list(neworders.keys())
 
 
 
