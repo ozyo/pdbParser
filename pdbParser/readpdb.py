4de6b9d08272fbf28571e182aecd3670578175e1
Ozge Yoluk <ozgeyoluk86@gmail.com>
2021-03-27 21:01:24 -0400

    Unit tests and bug fixes for readpdb

--- pdbParser/readpdb.py
+++ pdbParser/readpdb.py
@@ -1,6 +1,9 @@
 # See COPYING for license
 
 # We need to read quite a bit of information from the REMARKS lines. So it is better to keep them in a seperate module.
+from os import getcwd
+from pathlib import Path
+from typing import List
 import numpy as np
 import urllib.request, urllib.error, urllib.parse
 import logging
@@ -8,14 +11,19 @@ import logging
 FILTER_MOLTYPES = ["DNA", "RNA", "5'", "3'", "FAB", "ScFv"]
 
 
-def getpdb(pdbid, download=False):
+def getpdb(pdbid: Path, download: bool = False, clean: bool = False, cwd: Path = Path(getcwd())) -> List[str]:
     """
     Download or read in PDB files.
     """
+    if pdbid.suffix != ".pdb":
+        pdbid = pdbid.with_suffix(".pdb")
     if download:
+        logging.info(f"Downloading {pdbid}")
         try:
-            urllib.request.urlretrieve("http://files.rcsb.org/download/%s.pdb" % pdbid, pdbid + ".pdb")
-            pdblines = open(pdbid + ".pdb", "r").readlines()
+            urllib.request.urlretrieve(f"http://files.rcsb.org/download/{pdbid}", f"{cwd}/{pdbid}")
+            pdblines = open(f"{pdbid}", "r").readlines()
+            if clean:
+                (cwd / pdbid).unlink()
             return pdblines
         except urllib.error.HTTPError as err:
             if err.code == 404:
@@ -23,18 +31,21 @@ def getpdb(pdbid, download=False):
             else:
                 raise ValueError("Cannot reach PDB database, unknown error")
     else:
-        logging.info(f"Opening file {pdbid}")
-        with open(pdbid, "r") as pdbfile:
-            pdblines = pdbfile.readlines()
-        return pdblines
+        if (cwd / pdbid).is_file():
+            logging.info(f"Opening file {pdbid}")
+            with open(cwd / pdbid, "r") as pdbfile:
+                pdblines = pdbfile.readlines()
+            return pdblines
+        else:
+            raise FileExistsError(f"{pdbid} not found in {cwd.absolute()}")
 
 
-def checkmulti(pdb):
+def check_multimodel(pdblines: List[str]) -> bool:
     """
     Check if there are multiple models in the PDB file.
     """
     count = 0
-    for line in pdb:
+    for line in pdblines:
         if line.startswith("MODEL"):
             count += 1
     if count > 1:
@@ -43,18 +54,18 @@ def checkmulti(pdb):
         return False
 
 
-def get_compound_info(pdb):
+def get_compound_info(pdblines: List[str]) -> List[str]:
     """
     Filter compound lines
     """
     compndlist = []
-    for line in pdb:
+    for line in pdblines:
         if "COMPND" in line:
             compndlist.append(line[10:-1].strip())
     return compndlist
 
 
-def get_molid_index(compndlist):
+def get_molid_index(compndlist: List[str]) -> List[int]:
     """
     Return indexes of molecule ids
     """
@@ -65,34 +76,39 @@ def get_molid_index(compndlist):
     return molin
 
 
-def check_moltype_filter(lines):
+def check_moltype_filter(compndlist: List[str]) -> bool:
     """
     Check if mol can be filtered.
     """
-    for line in lines:
+    for line in compndlist:
         if any(s in line for s in FILTER_MOLTYPES):
             return True
     return False
 
 
-def extract_relevant_molid(molin, compndlist):
+def extract_relevant_molid(molin: List[int], compndlist: List[str]):
     """
     Extract relevant molid, after filtering there must be a single MOLID remaining, otherwise an error is raised.
     """
     if len(molin) > 1:
         passing_mol = []
-        mols = {1: compndlist[molin[0] : molin[1]], 2: compndlist[molin[1] :]}
-        for mol, lines in mols.items():
+        for i in range(len(molin) - 1):
+            lines = compndlist[molin[i] : molin[i + 1]]
             if not check_moltype_filter(lines):
-                passing_mol.append(mol)
+                passing_mol.append(i)
+        # Check the last element
+        if not check_moltype_filter(compndlist[len(molin) :]):
+            passing_mol.append(i + 1)
         if len(passing_mol) != 1:
             raise ValueError("Current version cannot handle this pdb file, both interaction partners are proteins.")
-        return mols[passing_mol[0]]
+        if passing_mol[0] == len(molin) - 1:
+            return compndlist[molin[passing_mol[0]] :]
+        return compndlist[molin[passing_mol[0]] : molin[passing_mol[0] + 1]]
     else:
         return compndlist
 
 
-def extract_chains(mol_lines):
+def extract_chains(mol_lines: List[str]):
     """
     Extract chain ids that belongs to the molid.
     """
@@ -102,45 +118,45 @@ def extract_chains(mol_lines):
     raise ValueError("Cannot extract chain information.")
 
 
-def findchains(pdb):
+def find_chains(pdblines: List[str]) -> List[str]:
     """
     Find chains that belongs to the complex from COMPND lines.
     """
-    compndlist = get_compound_info(pdb)
+    compndlist = get_compound_info(pdblines)
     molin = get_molid_index(compndlist)
     relevant_compounds = extract_relevant_molid(molin, compndlist)
     return extract_chains(relevant_compounds)
 
 
-def extract_missing_residues(pdb, chlist):
+# TODO: It's a little dangerous to assume that missing residues start exactly at 7th element.
+# This will break for any unconventional PDB file with missing missing residue lines.
+def extract_missing_residues(pdblines: List[str], chlist: List[str]) -> np.recarray:
     """
     Extracts missing residues.
     """
     # Where this function is used again?
     remark = []
-    for line in pdb:
+    for line in pdblines:
         if "REMARK 465" in line:
             remark.append(line)
-    r465 = np.genfromtxt(
-        remark[7:-1], names=["REMARK", "465", "rname", "ch", "rid"], dtype=["U6", int, "U3", "U1", int]
-    )
+    r465 = np.genfromtxt(remark[7:], names=["REMARK", "465", "rname", "ch", "rid"], dtype=["U6", int, "U3", "U1", int])
     filt = r465[np.in1d(r465["ch"], chlist)]
     return filt
 
 
-def read_atom(pdb):
+def read_atom(pdblines: List[str]) -> List[str]:
     """
     Return atom lines. Reading heteroatom lines might present other complications like mistaking calcium atoms for Calpha.
     For this reason, currently we are only reading atom lines.
     """
     atoms = []
-    for line in pdb:
+    for line in pdblines:
         if line[0:6].strip() in ["ATOM"]:
             atoms.append(line)
     return atoms
 
 
-def coord(atomlines):
+def coord(atomlines: List[str]) -> np.recarray:
     """
     Create a coordinate array from atomlines.
     """
@@ -156,7 +172,7 @@ def coord(atomlines):
         x = float(atom[30:38].strip())
         y = float(atom[38:46].strip())
         z = float(atom[46:54].strip())
-        if len(atom) == 79:
+        if len(atom) >= 79:
             occu = float(atom[54:61].strip())
             tfact = float(atom[61:66].strip())
             element = atom[76:78].strip()
---
+++ pdbParser/tests/test_readpdb.py
@@ -0,0 +1,202 @@
+from pathlib import Path
+import pytest
+import numpy as np
+
+from pdbParser.readpdb import (
+    FILTER_MOLTYPES,
+    getpdb,
+    check_multimodel,
+    get_compound_info,
+    get_molid_index,
+    check_moltype_filter,
+    extract_relevant_molid,
+    extract_chains,
+    find_chains,
+    extract_missing_residues,
+    read_atom,
+    coord,
+)
+
+DATA_DIR = Path(__file__).resolve().parents[0] / "test_data"
+
+TEST_PDB = open(f"{DATA_DIR}/1BA2.pdb").readlines()
+
+COMPND_SINGLE = [
+    "MOL_ID: 1;",
+    "MOLECULE: D-RIBOSE-BINDING PROTEIN;",
+    "CHAIN: A, B;",
+    "ENGINEERED: YES;",
+    "MUTATION: YES",
+]
+
+COMPND_MULTI_TEMPLATE = [
+    "MOL_ID: 1;",
+    "MOLECULE: D-RIBOSE-BINDING PROTEIN;",
+    "CHAIN: A, B;",
+    "ENGINEERED: YES;",
+    "MUTATION: YES",
+    "MOL_ID: 2;",
+    "MOLECULE: %s;",
+    "CHAIN: C,D;",
+]
+
+MISSING_RESIDUES = [
+    "REMARK 465 MISSING RESIDUES",
+    "REMARK 465 THE FOLLOWING RESIDUES WERE NOT LOCATED IN THE",
+    "REMARK 465 EXPERIMENT. (M=MODEL NUMBER; RES=RESIDUE NAME; C=CHAIN",
+    "REMARK 465 IDENTIFIER; SSSEQ=SEQUENCE NUMBER; I=INSERTION CODE.)",
+    "REMARK 465",
+    "REMARK 465   M RES C SSSEQI",
+    "REMARK 465     ILE A   103",
+    "REMARK 465     ASP A   104",
+    "REMARK 465     LYS B   105",
+    "REMARK 465     HIS B   341",
+    "REMARK 465     HIS C   342",
+]
+
+LAST_ATOMS_TEST_PDB = [
+    "ATOM   4005  O   GLN B 271     -20.348   4.657   4.099  1.00 31.34           O  \n",
+    "ATOM   4006  CB  GLN B 271     -20.068   3.067   6.959  1.00 30.59           C  \n",
+    "ATOM   4007  CG  GLN B 271     -20.350   1.718   7.641  1.00 30.95           C  \n",
+    "ATOM   4008  CD  GLN B 271     -18.966   1.143   7.945  1.00 32.20           C  \n",
+    "ATOM   4009  OE1 GLN B 271     -18.243   1.762   8.735  1.00 32.58           O  \n",
+    "ATOM   4010  NE2 GLN B 271     -18.646   0.036   7.286  1.00 32.60           N  \n",
+    "ATOM   4011  OXT GLN B 271     -21.819   3.067   4.468  1.00 31.48           O  \n",
+]
+
+LAST_ATOMS_TEST_PDB_SHORT = [i[:60] for i in LAST_ATOMS_TEST_PDB]
+
+COORDS = np.array(
+    [
+        (4005, "O", "", "GLN", "B", 271, "", -20.348, 4.657, 4.099, 1.00, 31.34, "O", ""),
+        (4006, "CB", "", "GLN", "B", 271, "", -20.068, 3.067, 6.959, 1.00, 30.59, "C", ""),
+        (4007, "CG", "", "GLN", "B", 271, "", -20.350, 1.718, 7.641, 1.00, 30.95, "C", ""),
+        (4008, "CD", "", "GLN", "B", 271, "", -18.966, 1.143, 7.945, 1.00, 32.20, "C", ""),
+        (4009, "OE1", "", "GLN", "B", 271, "", -18.243, 1.762, 8.735, 1.00, 32.58, "O", ""),
+        (4010, "NE2", "", "GLN", "B", 271, "", -18.646, 0.036, 7.286, 1.00, 32.60, "N", ""),
+        (4011, "OXT", "", "GLN", "B", 271, "", -21.819, 3.067, 4.468, 1.00, 31.48, "O", ""),
+    ],
+    dtype=("i,U4,U4,U4,U4,i,U4,f,f,f,f,f,U4,U4"),
+)
+
+COORDS_SHORT = np.array(
+    [
+        (4005, "O", "", "GLN", "B", 271, "", -20.348, 4.657, 4.099, float(), float(), "", ""),
+        (4006, "CB", "", "GLN", "B", 271, "", -20.068, 3.067, 6.959, float(), float(), "", ""),
+        (4007, "CG", "", "GLN", "B", 271, "", -20.350, 1.718, 7.641, float(), float(), "", ""),
+        (4008, "CD", "", "GLN", "B", 271, "", -18.966, 1.143, 7.945, float(), float(), "", ""),
+        (4009, "OE1", "", "GLN", "B", 271, "", -18.243, 1.762, 8.735, float(), float(), "", ""),
+        (4010, "NE2", "", "GLN", "B", 271, "", -18.646, 0.036, 7.286, float(), float(), "", ""),
+        (4011, "OXT", "", "GLN", "B", 271, "", -21.819, 3.067, 4.468, float(), float(), "", ""),
+    ],
+    dtype=("i,U4,U4,U4,U4,i,U4,f,f,f,f,f,U4,U4"),
+)
+
+COORDS.dtype.names = (
+    "atnr",
+    "atname",
+    "altloc",
+    "resname",
+    "ch",
+    "resnr",
+    "icode",
+    "x",
+    "y",
+    "z",
+    "occu",
+    "tfact",
+    "element",
+    "charge",
+)
+
+COORDS_SHORT.dtype.names = (
+    "atnr",
+    "atname",
+    "altloc",
+    "resname",
+    "ch",
+    "resnr",
+    "icode",
+    "x",
+    "y",
+    "z",
+    "occu",
+    "tfact",
+    "element",
+    "charge",
+)
+
+
+def test_getpdb():
+    assert getpdb(Path("1BA2"), download=True, clean=True) == TEST_PDB
+    assert getpdb(Path("1BA2"), cwd=DATA_DIR) == TEST_PDB
+
+
+@pytest.mark.parametrize(
+    "pdblines,result", [(TEST_PDB, False), (["MODEL", "MODEL"], True), (["MODEL"] + TEST_PDB, False)]
+)
+def test_check_multimodel(pdblines, result):
+    assert check_multimodel(pdblines) == result
+
+
+def test_get_compound_info():
+    assert get_compound_info(TEST_PDB) == COMPND_SINGLE
+
+
+def test_get_molid_index():
+    assert get_molid_index(COMPND_MULTI_TEMPLATE) == [0, 5]
+
+
+def yield_compnd():
+    for mol_type in FILTER_MOLTYPES:
+        tmp = []
+        for i in COMPND_MULTI_TEMPLATE:
+            if "%" in i:
+                tmp.append(i % mol_type)
+            else:
+                tmp.append(i)
+        return tmp
+
+
+@pytest.mark.parametrize("lines,result", [(yield_compnd(), True), (COMPND_SINGLE, False)])
+def test_check_moltype_filter(lines, result):
+    assert check_moltype_filter(lines) == result
+
+
+# TODO: Add tests where this function will raise an error.
+@pytest.mark.parametrize("lines,mol_index,result", [(yield_compnd(), [0, 5], COMPND_SINGLE)])
+def test_extract_relevant_molid(lines, mol_index, result):
+    assert extract_relevant_molid(mol_index, lines) == result
+
+
+def test_extract_chains():
+    assert extract_chains(COMPND_SINGLE) == ["A", "B"]
+
+
+# TODO: Add tests where sub fuctions will raise an error.
+def test_find_chains():
+    assert find_chains(TEST_PDB) == ["A", "B"]
+
+
+def gen_missing_array(lines):
+    return np.genfromtxt(lines, names=["REMARK", "465", "rname", "ch", "rid"], dtype=["U6", int, "U3", "U1", int])
+
+
+@pytest.mark.parametrize(
+    "lines,chids,result",
+    [
+        (TEST_PDB, ["A", "B"], gen_missing_array([])),
+        ((TEST_PDB + MISSING_RESIDUES), ["A", "B"], gen_missing_array(MISSING_RESIDUES[7:-1])),
+    ],
+)
+def test_extract_missing_residues(lines, chids, result):
+    np.testing.assert_array_equal(extract_missing_residues(lines, chids), result)
+
+
+def test_read_atom():
+    assert read_atom(TEST_PDB)[-7:] == LAST_ATOMS_TEST_PDB
+
+
+def test_coord():
+    np.testing.assert_array_equal(coord(LAST_ATOMS_TEST_PDB), COORDS, verbose=True)
+    np.testing.assert_array_equal(coord(LAST_ATOMS_TEST_PDB_SHORT), COORDS_SHORT, verbose=True)
